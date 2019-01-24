97d83c2f2839e662a056b4420e06ea1407361293
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-01-24 11:29:01 +0000

    feat: Create product data nodes for Gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,31 @@
 const { createClient } = require('@moltin/request')
 
-exports.sourceNodes = async ({ actions }, { client_id }) => {
+exports.sourceNodes = async (
+  { actions, createNodeId, createContentDigest },
+  { client_id }
+) => {
   const { createNode } = actions
 
   const moltin = new createClient({ client_id })
 
-  moltin
-    .get('products')
-    .then(console.log)
-    .catch(console.error)
+  const processProduct = product => {
+    const nodeId = createNodeId(`moltin-product-${product.id}`)
+    const nodeContent = JSON.stringify(product)
+    const nodeData = Object.assign({}, product, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `MoltinProduct`,
+        content: nodeContent,
+        contentDigest: createContentDigest(product)
+      }
+    })
 
-  return
+    return nodeData
+  }
+
+  const { data: products } = await moltin.get('products')
+
+  return products.forEach(product => createNode(processProduct(product)))
 }
