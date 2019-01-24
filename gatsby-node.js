b6dea306d8be274a1ff971d01dc331ece2dceae4
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-01-24 22:53:39 +0000

    feat: Create product main image nodes from included data
    Add linked field to product nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createClient } = require('@moltin/request')
+const { createRemoteFileNode } = require('gatsby-source-filesystem')
 
 exports.sourceNodes = async (
   { actions, createNodeId, createContentDigest },
@@ -8,10 +9,29 @@ exports.sourceNodes = async (
 
   const moltin = new createClient({ client_id })
 
-  const processProduct = product => {
+  const processProduct = ({ product, main_images }) => {
     const nodeId = createNodeId(`moltin-product-${product.id}`)
     const nodeContent = JSON.stringify(product)
-    const nodeData = Object.assign({}, product, {
+
+    let mainImage
+
+    if (product.relationships.main_image) {
+      const {
+        id,
+        links,
+        type,
+        link: { href },
+        ...rest
+      } = main_images.find(
+        main_image => main_image.id === product.relationships.main_image.data.id
+      )
+
+      mainImage = { href, ...rest }
+    }
+
+    const nodeData = {
+      ...product,
+      mainImage,
       id: nodeId,
       parent: null,
       children: [],
@@ -20,12 +40,45 @@ exports.sourceNodes = async (
         content: nodeContent,
         contentDigest: createContentDigest(product)
       }
-    })
+    }
 
     return nodeData
   }
 
-  const { data: products } = await moltin.get('products')
+  const {
+    data: products,
+    included: { main_images }
+  } = await moltin.get('products?include=main_image')
 
-  return products.forEach(product => createNode(processProduct(product)))
+  return products.forEach(async product =>
+    createNode(await processProduct({ product, main_images }))
+  )
+}
+
+exports.onCreateNode = async ({
+  node,
+  actions,
+  store,
+  cache,
+  createNodeId
+}) => {
+  const { createNodeField, createNode } = actions
+
+  if (node.internal.type === `MoltinProduct` && node.mainImage) {
+    const mainImageNode = await createRemoteFileNode({
+      url: node.mainImage.href,
+      store,
+      cache,
+      createNode,
+      createNodeId
+    })
+
+    if (mainImageNode) {
+      createNodeField({
+        node,
+        name: `linkToMainImage___NODE`,
+        value: mainImageNode.id
+      })
+    }
+  }
 }
