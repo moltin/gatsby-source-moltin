e23c380e23fe5e0bf786e6a481bd4ac8be797848
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-02-08 11:01:51 +0000

    feat: Category nodes (#3)
    * feat: Build nodes for category resources
    
    * feat: Add categories to product nodes
    
    * refactor: Don't use createNodeId for product node id value

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,17 +2,17 @@ const { createClient } = require('@moltin/request')
 const { createRemoteFileNode } = require('gatsby-source-filesystem')
 
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest },
+  { actions, createContentDigest },
   { client_id }
 ) => {
   const { createNode } = actions
 
   const moltin = new createClient({ client_id })
 
-  const processProduct = ({ product, main_images }) => {
-    const nodeId = createNodeId(`moltin-product-${product.id}`)
+  const processProduct = ({ product, main_images, categories }) => {
     const nodeContent = JSON.stringify(product)
 
+    let categoriesArray
     let mainImageHref
 
     if (product.relationships.main_image) {
@@ -25,10 +25,27 @@ exports.sourceNodes = async (
       mainImageHref = href
     }
 
+    if (product.relationships.categories) {
+      categoriesArray = product.relationships.categories.data.map(
+        relationship => {
+          const category = categories.find(
+            category => category.id === relationship.id
+          )
+
+          if (category) {
+            return {
+              ...category
+            }
+          }
+        }
+      )
+    }
+
     const nodeData = {
       ...product,
+      id: product.id,
+      categories: categoriesArray,
       mainImageHref,
-      id: nodeId,
       parent: null,
       children: [],
       internal: {
@@ -41,13 +58,35 @@ exports.sourceNodes = async (
     return nodeData
   }
 
+  const processCategory = ({ category }) => {
+    const nodeContent = JSON.stringify(category)
+
+    const nodeData = {
+      ...category,
+      id: category.id,
+      parent: null,
+      children: [],
+      internal: {
+        type: `MoltinCategory`,
+        content: nodeContent,
+        contentDigest: createContentDigest(category)
+      }
+    }
+
+    return nodeData
+  }
+
+  const { data: categories } = await moltin.get('categories')
   const {
     data: products,
     included: { main_images }
   } = await moltin.get('products?include=main_image')
 
-  return products.forEach(async product =>
-    createNode(await processProduct({ product, main_images }))
+  categories.forEach(async category =>
+    createNode(await processCategory({ category }))
+  )
+  products.forEach(async product =>
+    createNode(await processProduct({ product, main_images, categories }))
   )
 }
 
